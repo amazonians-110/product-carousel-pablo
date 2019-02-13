b401a93acf8ab51af4cdbd31674aaac1f39340df
Pablo Frias <pafrias90@gmail.com>
2019-02-13 11:40:24 -0800

    Query database for product info on mount

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,17 +14,16 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    // this.fetchProducts();
-    this.setState({ products: dummyData });
+    this.fetchProducts();
   }
 
   fetchProducts() {
-    axios.get('localhost:3007/category/product/05')
-      .then((data) => {
+    axios.get('/category/05')
+      .then(({ data }) => {
         this.setState({ products: data });
       })
       .catch((err) => {
-        // console.log(err);
+        console.log(err);
       });
   }
 
@@ -47,28 +46,28 @@ class App extends React.Component {
 }
 
 
-const dummyData = [
-  {
-    id: 6,
-    name: 'Dogfood',
-    image: 'https://images-na.ssl-images-amazon.com/images/I/81DNz45J01L._AC_UL160_SR160,160_.jpg',
-    price: '12.99',
-    avgReview: '4.2',
-    isPrime: 'f',
-    category: 'pet',
-    manufacturer: 'Purina',
-  },
-  {
-    id: 5,
-    name: 'Laptop',
-    image: 'https://images-na.ssl-images-amazon.com/images/G/01/pc/ac/es-home-white.jpg',
-    price: '99.99',
-    avgReview: '3.7',
-    isPrime: 't',
-    category: 'electronics',
-    manufacturer: 'Acer',
-  },
-];
+// const dummyData = [
+//   {
+//     id: 6,
+//     name: 'Dogfood',
+//     image: 'https://images-na.ssl-images-amazon.com/images/I/81DNz45J01L._AC_UL160_SR160,160_.jpg',
+//     price: '12.99',
+//     avgReview: '4.2',
+//     isPrime: 'f',
+//     category: 'pet',
+//     manufacturer: 'Purina',
+//   },
+//   {
+//     id: 5,
+//     name: 'Laptop',
+//     image: 'https://images-na.ssl-images-amazon.com/images/G/01/pc/ac/es-home-white.jpg',
+//     price: '99.99',
+//     avgReview: '3.7',
+//     isPrime: 't',
+//     category: 'electronics',
+//     manufacturer: 'Acer',
+//   },
+// ];
 
 // eslint-disable-next-line no-undef
 ReactDOM.render(<App />, document.getElementById('app'));
--- server/controllers/router.js
+++ server/controllers/router.js
@@ -1,13 +1,21 @@
 const db = require('../database/');
 
-module.exports = {
-  post: (req, res) => {
-    res.status(201).send();
-  },
+const randomRelations = (num, limit) => {
+  const results = [];
+  while (results.length < num) {
+    const rand = Math.floor(Math.random() * limit);
+    if (!results.includes(rand)) {
+      results.push(rand);
+    }
+  }
+  return results;
+};
 
-  get: (req, res) => {
-    const { productId } = req.params;
-    db.select('*').from('products').where('product_id', productId)
+module.exports = {
+  // add search by category
+  readDefault: (req, res) => {
+    const relatedProducts = randomRelations(15, 100);
+    db.select('*').from('products').whereIn('product_id', relatedProducts)
       .then((data) => {
         res.status(200).send(data);
       })
@@ -16,8 +24,17 @@ module.exports = {
       });
   },
 
-  dbtest: (req, res) => {
-    // db
-    res.status(200).send(dummyData);
-  },
+  // read: (req, res) => {
+  //   const { productId, relationship } = req.params;
+  //   db.select('*').from('products').where('product_id', productId)
+  //     .then((data) => {
+  //       res.status(200).send(data);
+  //     })
+  //     .catch((err) => {
+  //       res.status(400).send(err);
+  //     });
+  // },
+  // post: (req, res) => {
+  //   res.status(201).send();
+  // },
 };
