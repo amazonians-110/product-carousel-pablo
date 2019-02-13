cd0113fd24f8253811f0e0f747644ae5a04a6eec
Pablo Frias <pafrias90@gmail.com>
2019-02-13 08:03:29 -0800

    Implement table creation through seed file

--- server/helpers/seed.js
+++ server/helpers/seed.js
@@ -1,8 +1,18 @@
-const pg = require('knex')({
-	client: 'pg',
-	connection: {
-		host: '127.0.0.1',
-		database: 'amazon',
-	}
-});
+const db = require('../database');
+
+db.schema.dropTableIfExists('products')
+  .then(() => db.schema.createTable('products', (table) => {
+    table.increments('product_id');
+    table.string('name');
+    table.string('image');
+    table.decimal('price');
+    table.decimal('avg_review', 2, 1);
+    table.integer('review_count');
+    table.boolean('is_prime');
+    table.string('category');
+    table.string('manufacturer');
+  }))
+  .then(() => {
+
+  });
 
