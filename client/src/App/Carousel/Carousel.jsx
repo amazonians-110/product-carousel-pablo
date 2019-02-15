1b35dbd7b15d8752b2a022dfb445821c23083fcd
Pablo Frias <pafrias90@gmail.com>
2019-02-15 14:17:48 -0800

    Implement page scrolling and page links

--- client/src/App/Carousel/Carousel.jsx
+++ client/src/App/Carousel/Carousel.jsx
@@ -5,13 +5,13 @@ import Product from './Product/Product.jsx';
 import Button from './Button/Button.jsx';
 import carousel from './Carousel.css';
 
-
-const Carousel = ({ products, scroll, click }) => (
+/* click */
+const Carousel = ({ products, scroll }) => (
   <div className={carousel.container}>
 
     <Button scroll={scroll} direction="left" />
     {products
-      ? products.map(product => <Product key={product.id} click={click} product={product} />)
+      ? products.map(product => <Product key={product.id} product={product} />)
       : null }
     <Button scroll={scroll} direction="right" />
   </div>
@@ -20,7 +20,7 @@ const Carousel = ({ products, scroll, click }) => (
 Carousel.propTypes = {
   products: propTypes.arrayOf(propTypes.object).isRequired,
   scroll: propTypes.func.isRequired,
-  click: propTypes.func.isRequired,
+  // click: propTypes.func.isRequired,
 };
 
 export default Carousel;
