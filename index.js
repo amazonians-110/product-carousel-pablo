287dd68871cd4e3ae88a306f319da0143087fd7a
Pablo Frias <pafrias90@gmail.com>
2019-02-23 18:54:24 -0800

    Bugfix server route paths for bundle

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
 const express = require('express');
-<<<<<<< HEAD
-=======
 const morgan = require('morgan');
->>>>>>> feature-junctions
 const cors = require('cors');
 const compression = require('compression');
 const router = require('./server/controllers/router.js');
 
 const app = express();
 
-<<<<<<< HEAD
-=======
 app.use(morgan('tiny'));
 
->>>>>>> feature-junctions
 app.use(cors());
 
 app.use(compression());
 
-app.use(express.static(`${__dirname}/dist`));
+app.get('/api/bundle.js', (req, res) => {
+  res.sendFile(`${__dirname}/dist/bundle.js`);
+});
 
 app.get('/api/bundle', (req, res) => {
   res.sendFile(`${__dirname}/dist/bundle.js`);
 });
+
+app.use(express.static(`${__dirname}/dist`));
 
 app.get('/api/products/:productId/', router.readRelationship);
 
